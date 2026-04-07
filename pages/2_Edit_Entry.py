24540ff31b5a2465dba71fd082bb4f51808f7a3a
mrcneumeyer-collab <mrcneumeyer@gmail.com>
2026-04-07 16:11:30 -0700

    Update 2_Edit_Entry.py

--- pages/2_Edit_Entry.py
+++ pages/2_Edit_Entry.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.set_page_config(page_title="Edit Food Entry", page_icon="✏️")
 
 def get_connection():
-    return psycopg2.connect(st.secrets["URL_DB1"])
+    return psycopg2.connect(st.secrets["URL_DB"])
 
 st.title("✏️ Edit a Food Entry")
 
